19fa8f9e4387b6a5f301d517581ef0e52799d4b8
Leo Gao <leogao31@gmail.com>
2021-05-23 00:08:32 -0600

    add icecream import

--- pyfra/__init__.py
+++ pyfra/__init__.py
@@ -6,3 +6,9 @@ from .functional import *
 from .remote import *
 from .web import *
 from .contrib import *
+
+# fallback snippet from https://github.com/gruns/icecream
+try:
+    from icecream import ic
+except ImportError:  # Graceful fallback if IceCream isn't installed.
+    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa
