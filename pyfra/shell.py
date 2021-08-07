77fc714d96b7d6e8c2047ceb39a775dce79d5246
Leo Gao <leogao31@gmail.com>
2021-08-07 00:12:00 -0600

    shell: swap python alias for function

--- pyfra/shell.py
+++ pyfra/shell.py
@@ -27,7 +27,7 @@ def _wrap_command(x, no_venv=False, pyenv_version=None):
     bashrc_payload = r"""import sys,re; print(re.sub("If not running interactively.{,128}?esac", "", sys.stdin.read(), flags=re.DOTALL).replace('[ -z "$PS1" ] && return', ''))"""
     hdr = f"shopt -s expand_aliases; ctrlc() {{ echo Shell wrapper interrupted with C-c, raising error; exit 174; }}; trap ctrlc SIGINT; "
     hdr += f"eval \"$(cat ~/.bashrc | python3 -c {bashrc_payload | quote})\"  > /dev/null 2>&1; "
-    hdr += "alias python=python3 ; " # use python3 by default
+    hdr += "python() { python3 \"$@\"; };" # use python3 by default
     if pyenv_version is not None: hdr += f"pyenv shell {pyenv_version} || exit 1 > /dev/null 2>&1; "
     if not no_venv: hdr += "[ -f env/bin/activate ] && . env/bin/activate; "
     return hdr + x
