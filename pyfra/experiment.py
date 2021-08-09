35a0815685cc0575dc6c8d5727a5b06b86a9227e
Leo Gao <leogao31@gmail.com>
2021-08-09 15:20:26 -0600

    experiment: delegation ignore files

--- pyfra/experiment.py
+++ pyfra/experiment.py
@@ -1,6 +1,7 @@
 import os
 from shlex import quote
 import sys
+import pathlib
 
 import pyfra.shell
 import pyfra.remote
@@ -19,24 +20,26 @@ class Experiment:
         return r
 
     def delegate(self, rem):
+        tmux_name = f"pyfra_delegated_{self.experiment_name}"
         if isinstance(rem, str): rem = self.remote(rem)
     
         if "PYFRA_DELEGATED" in os.environ:
             return
         
-        env = rem.env(self.experiment_name)
+        env = rem.env(tmux_name)
 
-        pyfra.shell.copy(pyfra.remote.local.path("."), env.path("."), into=False)
+        ignore = [] if not pathlib.Path(".pyfraignore").exists() else pathlib.Path(".pyfraignore").read_text().strip().splitlines()
+        pyfra.shell.copy(pyfra.remote.local.path("."), env.path("."), into=False, exclude=ignore)
 
         def _attach_tmux():
-            env.sh(f"tmux a -t {quote(self.experiment_name)}", maxbuflen=0, forward_keys=True)
+            env.sh(f"tmux a -t {quote(tmux_name)}", maxbuflen=0, forward_keys=True)
         
         try:
             _attach_tmux()
         except pyfra.shell.ShellException:
-            env.sh(f"sudo apt install tmux -y; pip install -U pyfra; pip install -r requirements.txt; tmux new-session -d -s {quote(self.experiment_name)}", ignore_errors=True)
+            env.sh(f"sudo apt install tmux -y; pip install -U pyfra; pip install -r requirements.txt; tmux new-session -d -s {quote(tmux_name)}", ignore_errors=True)
             cmd = pyfra.shell._wrap_command("eval $(tmux show-env -s |grep '^SSH_'); PYFRA_DELEGATED=1 python "+" ".join([quote(x) for x in sys.argv]), pyenv_version=env.pyenv_version)
-            env.sh(f"tmux send-keys -t {quote(self.experiment_name)} {quote(cmd)} Enter")
+            env.sh(f"tmux send-keys -t {quote(tmux_name)} {quote(cmd)} Enter")
             _attach_tmux()
 
         sys.exit(0)
